9f4a68fb71e2b531271dc033b12dce4ade977f7a
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-12 13:41:28 +0400

    t1

--- includes/featuredProd-slider.php
+++ includes/featuredProd-slider.php
@@ -1,1 +1,20 @@
-Done!
+<?php
+$args = array(
+    'post_type' => 'featured_products',
+    'order' => 'ASC',
+    'orderby' => 'ID'
+);
+$loop = new WP_Query($args); ?>
+
+
+<div id="featured-products-carousel" class="featured-product-carousel">
+
+<?php if ($loop->have_posts()) : ?>
+        <?php while ($loop->have_posts()) :
+            $loop->the_post(); ?>
+
+<?php the_title( ) ?>
+
+     <?php endwhile; ?>
+    <?php endif; ?>
+</div>
