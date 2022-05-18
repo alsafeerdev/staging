8bb54f064710289c62d26310331f91ed29dcdf5e
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-18 13:48:05 +0400

    Update ajax-portfolioGrid.php

--- includes/ajax-portfolioGrid.php
+++ includes/ajax-portfolioGrid.php
@@ -2,8 +2,9 @@
 
 $terms = get_terms( 'industrygroups' );
 
-echo '<div class="row">';
 
+echo '<div class="container catergory-grid">';
+echo   ' <div class="row">';
 foreach ( $terms as $term ) {
 
     // The $term is an object, so we don't need to specify the $taxonomy.
@@ -14,23 +15,21 @@ foreach ( $terms as $term ) {
         continue;
     }
 
-    // We successfully got a link. Print it out.
-    // echo '<div class="col-lg-3"><a href="' . esc_url( $term_link ) . '">' . $term->name . '</a></div>';
-}
-
-// echo '</div>';
-
-?>
-
-<div class="container catergory-grid">
-    <div class="row">
-        <a href="<?php $term_link ?>" class="wrapper-link">
+  echo    '   <a href="' . $term_link .' " class="wrapper-link">
             <div class="col-lg-4 portfolio-catergory-grid-item-container">
                 <div class="portfolio-catergory-grid-img-container">
                     <img src="/wp-content/uploads/2022/04/Customized-USB-Featured-IMG.jpg" alt="" srcset="">
                 </div>
-                <h3 class="portfolio-catergory-grid"><?php $term->name ?> </h3>
+                <h3 class="portfolio-catergory-grid">' . php $term->name . '</h3>
             </div>
-        </a>
-    </div>
-</div>
+        </a>';
+
+    // We successfully got a link. Print it out.
+    echo '<div class="col-lg-3"><a href="' . esc_url( $term_link ) . '">' . $term->name . '</a></div>';
+}
+
+echo '    </div>';
+echo '</div>';
+
+?>
+
