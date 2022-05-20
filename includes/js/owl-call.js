df22f9eea199676133de4a7ed46c623cb7f149d2
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-20 11:12:17 +0400

    dotsEach 2

--- includes/js/owl-call.js
+++ includes/js/owl-call.js
@@ -155,7 +155,7 @@ jQuery(document).ready(function () {
     margin: 10,
     center: true,
     dots: true,
-    dotsEach: 3,
+    dotsEach: 5,
     autoplay: true,
     nav: true,
     autoplayTimeout: 3000,
