81cccc43672733d5cad08a48835e91c1e8ffe1bc
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-20 10:12:07 +0400

    Update featuredProd-slider.php

--- includes/featuredProd-slider.php
+++ includes/featuredProd-slider.php
@@ -19,16 +19,16 @@ $loop = new WP_Query($args); ?>
             ?>
             <div class="feat-prods-container text-center">
                 <a href="<?php echo get_permalink(); ?>" class="feat-prods-wrapper-link" target="_blank">
-                <div class="feat-prods-img-container">
-                    <img src="<?php echo the_post_thumbnail_url('full'); ?>" alt="<?php echo $image_alt ?>" srcset="" class="feat-prods-feat-image">
-                </div>
-                <div class="feat-prods-content-container">
-                    <h3 class="feat-prods-title"> <?php the_title(); ?></h3>
-                    <p class="feat-prods-exerpt">
-                        <?php echo get_the_excerpt(); ?>
-                    </p>
-                    <p class="feat-prods-know-more">Learn More</p>
-                </div>
+                    <div class="feat-prods-img-container">
+                        <img src="<?php echo the_post_thumbnail_url('full'); ?>" alt="<?php echo $image_alt ?>" srcset="" class="feat-prods-feat-image">
+                    </div>
+                    <div class="feat-prods-content-container">
+                        <h3 class="feat-prods-title"> <?php the_title(); ?></h3>
+                        <p class="feat-prods-exerpt">
+                            <?php echo get_the_excerpt(); ?>
+                        </p>
+                        <p class="feat-prods-know-more">Learn More</p>
+                    </div>
                 </a>
             </div>
         <?php endwhile; ?>
