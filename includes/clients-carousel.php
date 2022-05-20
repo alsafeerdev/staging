08e73ec3194d5e3cdee110239bc92d214de20a4f
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-20 10:31:25 +0400

    Update clients-carousel.php

--- includes/clients-carousel.php
+++ includes/clients-carousel.php
@@ -4,14 +4,33 @@ $args = array(
 );
 $loop = new WP_Query($args); ?>
 
-<?php if ($loop->have_posts()) : ?>
-    <?php while ($loop->have_posts()) :
-        $loop->the_post(); ?>
-        <p><?php the_title(); ?></p>
-    <?php endwhile; ?>
-<?php endif; ?>
+<div id="clients-carousel" class="owl-carousel owl-theme clients-carousel">
 
-
+    <?php if ($loop->have_posts()) : ?>
+        <?php while ($loop->have_posts()) :
+            $loop->the_post(); ?>
+            <?php $feat_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>
+            <?php
+            $image_id = get_post_thumbnail_id();
+            $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
+            ?>
+            <div class="feat-prods-container text-center">
+                <a href="<?php echo get_permalink(); ?>" class="feat-prods-wrapper-link" target="_blank">
+                    <div class="feat-prods-img-container">
+                        <img src="<?php echo the_post_thumbnail_url('full'); ?>" alt="<?php echo $image_alt ?>" srcset="" class="feat-prods-feat-image">
+                    </div>
+                    <div class="feat-prods-content-container">
+                        <h3 class="feat-prods-title"> <?php the_title(); ?></h3>
+                        <p class="feat-prods-exerpt">
+                            <?php echo get_the_excerpt(); ?>
+                        </p>
+                        <p class="feat-prods-know-more">Learn More</p>
+                    </div>
+                </a>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+</div>
 
 
 
