3934e909b2c22731657abe7f4d74c3b1d5b2f207
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-22 12:37:18 +0400

    Update synced-slider.php

--- includes/synced-slider.php
+++ includes/synced-slider.php
@@ -1,1 +1,25 @@
-DONE!
+<?php
+$args = array(
+    'post_type' => 'portfolio_items',
+    'order' => 'ASC',
+    'orderby' => 'ID'
+);
+$loop = new WP_Query($args); ?>
+
+<div class="main-image-carousel">
+
+    <?php if ($loop->have_posts()) : ?>
+        <?php while ($loop->have_posts()) :
+            $loop->the_post(); ?>
+
+            <?php the_title() ?>
+
+
+        <?php endwhile; ?>
+    <?php endif; ?>
+
+</div>
+
+
+
+<?php wp_reset_postdata(); ?>
