dfbb856614d1447745a76d54416dee12a5e769aa
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-22 14:20:10 +0400

    Update synced-slider.php

--- includes/synced-slider.php
+++ includes/synced-slider.php
@@ -3,12 +3,9 @@ $images = get_field('gallery_items');
 $size = 'full'; // (thumbnail, medium, large, full or custom size)
 if ($images) : ?>
 
-    <?php
-    $imgurl = get_field('gallery_items', $post->ID);
-    ?>
     <?php foreach ($images as $image_id) : ?>
 
-        <img src="<?php echo $imgurl ?>" alt="" srcset="">
+        <img src="<?php echo $images ?>" alt="" srcset="">
 
 
     <?php endforeach; ?>
