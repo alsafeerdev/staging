54b2d55195012482c8bc8287d00bd609395fa757
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-22 12:59:51 +0400

    Update synced-slider.php

--- includes/synced-slider.php
+++ includes/synced-slider.php
@@ -1,10 +1,20 @@
-<?php 
+<?php
 $images = get_field('gallery_items');
 $size = 'full'; // (thumbnail, medium, large, full or custom size)
-if( $images ): ?>
-        <?php foreach( $images as $image_id ): ?>
-            
-                <?php echo wp_get_attachment_image( $image_id, $size ); ?>
-            
-        <?php endforeach; ?>
+if ($images) : ?>
+
+    <?php
+    $imgurl = get_field('gallery_items', $post->ID);
+
+    if (filter_var($imgurl, FILTER_VALIDATE_URL) === FALSE) {
+        $imgurl = wp_get_attachment_url($imgurl);
+    }
+    echo '<img src="' . $imgurl . '" alt="image">';
+    ?>
+    <?php foreach ($images as $image_id) : ?>
+
+        <img src="<?php echo $imgurl ?>" alt="" srcset="">
+
+
+    <?php endforeach; ?>
 <?php endif; ?>
