0d341150066bdabf2ec0264144cde72b94ba3966
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-22 14:17:17 +0400

    Update synced-slider.php

--- includes/synced-slider.php
+++ includes/synced-slider.php
@@ -5,11 +5,6 @@ if ($images) : ?>
 
     <?php
     $imgurl = get_field('gallery_items', $post->ID);
-
-    if (filter_var($imgurl, FILTER_VALIDATE_URL) === FALSE) {
-        $imgurl = wp_get_attachment_url($imgurl);
-    }
-    echo '<img src="' . $imgurl . '" alt="image">';
     ?>
     <?php foreach ($images as $image_id) : ?>
 
