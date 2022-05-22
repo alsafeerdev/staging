b529e234cc4cf1eff9e6bc10b26bb606ba3f03f2
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-22 14:46:36 +0400

    Update synced-slider.php

--- includes/synced-slider.php
+++ includes/synced-slider.php
@@ -5,31 +5,25 @@ if ($images) : ?>
     <div class="synced-slider-wrapper">
         <!-- Big Image Carousel Start -->
         <div id="big" class="synced-slider-main-img owl-carousel owl-theme big-image-carousel">
-
-            <div class="big-image-carousel-item">
-
-                <?php foreach ($images as $image) : ?>
+            <?php foreach ($images as $image) : ?>
+                <div class="big-image-carousel-item">
 
                     <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 
-                <?php endforeach; ?>
-
-            </div>
-
+                </div>
+            <?php endforeach; ?>
         </div>
         <!-- Big Image Carousel End -->
         <!-- Thumbnail Carousel Start -->
         <div id="thumbs" class="owl-carousel owl-theme thumbnail-carousel">
 
-            <div class="thumbnail-carousel-item">
-
-                <?php foreach ($images as $image) : ?>
+            <?php foreach ($images as $image) : ?>
+                <div class="thumbnail-carousel-item">
 
                     <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="Thumbnail of <?php echo esc_url($image['alt']); ?>" />
 
-                <?php endforeach; ?>
-
-            </div>
+                </div>
+            <?php endforeach; ?>
 
         </div>
         <!-- Thumbnail Carousel End -->
