7bcc0360c155aa0f476a67de983d8f78a4762d2d
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-22 14:24:23 +0400

    Update synced-slider.php

--- includes/synced-slider.php
+++ includes/synced-slider.php
@@ -1,12 +1,23 @@
-<?php
+<?php 
 $images = get_field('gallery_items');
-$size = 'full'; // (thumbnail, medium, large, full or custom size)
-if ($images) : ?>
-
-    <?php foreach ($images as $image) : ?>
-
-        <img src="<?php echo $images ?>" alt="" srcset="">
-
-
-    <?php endforeach; ?>
+if( $images ): ?>
+    <div id="slider" class="flexslider">
+        <ul class="slides">
+            <?php foreach( $images as $image ): ?>
+                <li>
+                    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                    <p><?php echo esc_html($image['caption']); ?></p>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    <div id="carousel" class="flexslider">
+        <ul class="slides">
+            <?php foreach( $images as $image ): ?>
+                <li>
+                    <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="Thumbnail of <?php echo esc_url($image['alt']); ?>" />
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
 <?php endif; ?>
