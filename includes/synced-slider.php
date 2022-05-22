7d22865a4a2b89c9a29afd88674023584458489b
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-22 14:44:34 +0400

    Update synced-slider.php

--- includes/synced-slider.php
+++ includes/synced-slider.php
@@ -1,23 +1,38 @@
-<?php 
+<?php
 $images = get_field('gallery_items');
-if( $images ): ?>
-    <div id="slider" class="flexslider">
-        <ul class="slides">
-            <?php foreach( $images as $image ): ?>
-                <li>
+if ($images) : ?>
+
+    <div class="synced-slider-wrapper">
+        <!-- Big Image Carousel Start -->
+        <div id="big" class="synced-slider-main-img owl-carousel owl-theme big-image-carousel">
+
+            <div class="big-image-carousel-item">
+
+                <?php foreach ($images as $image) : ?>
+
                     <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                    <p><?php echo esc_html($image['caption']); ?></p>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-    <div id="carousel" class="flexslider">
-        <ul class="slides">
-            <?php foreach( $images as $image ): ?>
-                <li>
+
+                <?php endforeach; ?>
+
+            </div>
+
+        </div>
+        <!-- Big Image Carousel End -->
+        <!-- Thumbnail Carousel Start -->
+        <div id="thumbs" class="owl-carousel owl-theme thumbnail-carousel">
+
+            <div class="thumbnail-carousel-item">
+
+                <?php foreach ($images as $image) : ?>
+
                     <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="Thumbnail of <?php echo esc_url($image['alt']); ?>" />
-                </li>
-            <?php endforeach; ?>
-        </ul>
+
+                <?php endforeach; ?>
+
+            </div>
+
+        </div>
+        <!-- Thumbnail Carousel End -->
+
     </div>
 <?php endif; ?>
