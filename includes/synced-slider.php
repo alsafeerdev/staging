abc6b79b24b3dec836db59e7b1e310db07c2ab5d
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-22 12:54:26 +0400

    Update synced-slider.php

--- includes/synced-slider.php
+++ includes/synced-slider.php
@@ -2,11 +2,9 @@
 $images = get_field('gallery_items');
 $size = 'full'; // (thumbnail, medium, large, full or custom size)
 if( $images ): ?>
-    <ul>
         <?php foreach( $images as $image_id ): ?>
             
                 <?php echo wp_get_attachment_image( $image_id, $size ); ?>
             
         <?php endforeach; ?>
-    </ul>
 <?php endif; ?>
