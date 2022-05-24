2f50e6b2ce2517357d3270e1c8101c0aa5d39a6a
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-24 13:39:43 +0400

    Removed Pagination

--- functions.php
+++ functions.php
@@ -60,26 +60,6 @@ function product_grid()
 
 add_shortcode('custom-product-grid', 'product_grid');
 
-// Numeric Pagination
-function pagination_bar($custom_query)
-{
-    $total_pages = $custom_query->max_num_pages;
-    $big = 999999999; // need an unlikely integer
-
-    if ($total_pages > 1) {
-        $current_page = max(1, get_query_var('paged'));
-
-        echo paginate_links(array(
-            'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
-            'format' => '?paged=%#%',
-            'current' => $current_page,
-            'total' => $total_pages,
-            'prev_text' => 'Prev.',
-            'next_text' => 'Next',
-        ));
-    }
-}
-
 // Modify Exerpt Length
 
 function custom_excerpt_length($length)
