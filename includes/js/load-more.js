6b72c5811b324e164fe4c2ca734292934622c941
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-25 15:52:25 +0400

    loadmore

--- includes/js/load-more.js
+++ includes/js/load-more.js
@@ -1,34 +1,64 @@
-jQuery(function($){ // use jQuery code inside this to avoid "$ is not defined" error
-	$('.misha_loadmore').click(function(){
- 
-		var button = $(this),
-		    data = {
-			'action': 'loadmore',
-			'query': misha_loadmore_params.posts, // that's how we get params from wp_localize_script() function
-			'page' : misha_loadmore_params.current_page
-		};
- 
-		$.ajax({ // you can also use $.post here
-			url : misha_loadmore_params.ajaxurl, // AJAX handler
-			data : data,
-			type : 'POST',
-			beforeSend : function ( xhr ) {
-				button.text('Loading...'); // change the button text, you can also add a preloader image
-			},
-			success : function( data ){
-				if( data ) { 
-					button.text( 'More posts' ).prev().before(data); // insert new posts
-					misha_loadmore_params.current_page++;
- 
-					if ( misha_loadmore_params.current_page == misha_loadmore_params.max_page ) 
-						button.remove(); // if last page, remove the button
- 
-					// you can also fire the "post-load" event here if you use a plugin that requires it
-					// $( document.body ).trigger( 'post-load' );
-				} else {
-					button.remove(); // if no data, remove the button as well
-				}
-			}
-		});
-	});
+jQuery(function ($) {
+    var canBeLoaded = true, // this param allows to initiate the AJAX call only if necessary
+        bottomOffset = 2000; // the distance (in px) from the page bottom when you want to load more posts
+
+    $(window).scroll(function () {
+        var data = {
+            'action': 'loadmore',
+            'query': misha_loadmore_params.posts,
+            'page': misha_loadmore_params.current_page
+        };
+        if ($(document).scrollTop() > ($(document).height() - bottomOffset) && canBeLoaded == true) {
+            $.ajax({
+                url: misha_loadmore_params.ajaxurl,
+                data: data,
+                type: 'POST',
+                beforeSend: function (xhr) {
+                    // you can also add your own preloader here
+                    // you see, the AJAX call is in process, we shouldn't run it again until complete
+                    canBeLoaded = false;
+                },
+                success: function (data) {
+                    if (data) {
+                        $('#main').find('article:last-of-type').after(data); // where to insert posts
+                        canBeLoaded = true; // the ajax is completed, now we can run it again
+                        misha_loadmore_params.current_page++;
+                    }
+                }
+            });
+        }
+    });
 });
+
+function misha_loadmore_ajax_handler() {
+
+    // prepare our arguments for the query
+    $args = json_decode(stripslashes($_POST['query']), true);
+    $args['paged'] = $_POST['page'] + 1; // we need next page to be loaded
+    $args['post_status'] = 'publish';
+
+    // it is always better to use WP_Query but not here
+    query_posts($args);
+
+    if (have_posts()) :
+
+        // run the loop
+        while (have_posts()): the_post();
+
+    // look into your theme code how the posts are inserted, but you can use your own HTML of course
+    // do you remember? - my example is adapted for Twenty Seventeen theme
+    get_template_part('template-parts/post/content', get_post_format());
+    // for the test purposes comment the line above and uncomment the below one
+    // the_title();
+
+
+    endwhile;
+
+    endif;
+    die; // here we exit the script and even no wp_reset_query() required!
+}
+
+
+
+add_action('wp_ajax_loadmore', 'misha_loadmore_ajax_handler'); // wp_ajax_{action}
+add_action('wp_ajax_nopriv_loadmore', 'misha_loadmore_ajax_handler'); // wp_ajax_nopriv_{action}
