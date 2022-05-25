9a9f8d6bac583d78d236e751a3a4eb9e48646f06
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-25 17:14:16 +0400

    asad

--- includes/oldproductcat.php
+++ includes/oldproductcat.php
@@ -24,129 +24,83 @@
                         <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo">
                             <div class="accordion-body">
                                 <div class="accordion" id="filter-cat">
-                                    <?php
 
-                                    foreach (get_categories('hide_empty=0') as $cat) :
-                                        if (!$cat->parent) {
-                                            echo '<div class="accordion-item">';
-                                            echo '<h2 class="accordion-header" id="' . $cat->term_id . '">';
-                                            echo '
-                                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse' . $cat->term_id . '" aria-expanded="true" aria-controls="collapse' . $cat->term_id . '">
-                                                ' .  $cat->name . '';
-                                            echo '</button>';
-                                            echo '</h2>';
+                                    <div class="filter-wrapper">
+                                        <form action="<?php echo admin_url('admin-ajax.php') ?>" method="POST" id="filter">
+                                            <?php
+                                            if ($terms = get_terms(array(
+                                                'taxonomy' => 'product_categories',
+                                                'orderby' => 'name'
+                                            ))) : ?>
+                                                <div class="category-list">
+                                                    <?php foreach ($terms as $term) : ?>
+                                                        <label for="<?php echo $term->term_id ?>">
+                                                            <input type="radio" name="prod-category" id="<?php echo $term->term_id ?>" value="<?php echo $term->term_id ?>">
+                                                            <?php echo $term->name ?> (<?php echo $term->count ?>)
+                                                        </label>
 
-                                            echo '<div id="collapse' . $cat->term_id . '" class="accordion-collapse collapse" aria-labelledby="' . $cat->term_id . '" data-bs-parent="#filter-cat">';
-                                            echo ' <div class="accordion-body">';
-                                            process_cat_tree($cat->term_id);
-
-                                            echo '</div>';
-                                            echo '</div>';
-                                            echo '</div>';
-                                        }
-                                    endforeach;
-                                    wp_reset_query();
-
-                                    function process_cat_tree($cat)
-                                    {
-
-                                        $args = array('category__in' => array($cat), 'numberposts' => -1);
-                                        $cat_posts = get_posts($args);
-
-                                        if ($cat_posts) :
-                                            foreach ($cat_posts as $post) :
-                                                echo '<p>';
-                                                echo '<a href="' . get_permalink($post->ID) . '">' . $post->post_title . '</a>';
-                                                echo '</p>';
-                                            endforeach;
-                                        endif;
-
-                                        $next = get_categories('hide_empty=0&parent=' . $cat);
-
-                                        if ($next) :
-                                            foreach ($next as $cat) :
-                                                echo '<p>';
-                                                echo '<a href="' . get_permalink($post->ID) . '">' . $cat->name . '</a>';
-                                                echo '</p>';
-                                                process_cat_tree($cat->term_id);
-                                            endforeach;
-                                        endif;
-                                    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                    ?>
-
-
-
+                                                    <?php endforeach; ?>
+                                                </div>
+                                            <?php endif; ?>
+                                            <button class="submit-btn" type="submit">Apply Filter</button>
+                                            <input type="hidden" name="action" value="myfilter">
+                                        </form>
+                                    </div>
 
                                 </div>
-
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-9">
+            <div class="col-9">
 
-            <?php
-            $args = array(
-                'post_type' => 'services',
-                'posts_per_page' => -1,
-            );
-            $loop = new WP_Query($args); ?>
+                <?php
+                $args = array(
+                    'post_type' => 'services',
+                    'posts_per_page' => -1,
+                );
+                $loop = new WP_Query($args); ?>
 
-            <div class="row">
-                <?php if ($loop->have_posts()) : ?>
-                    <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-                        <div class="col-4 my-3">
-                            <div class="post-container">
-                                <div class="image-container">
-                                    <?php
-                                    // An attachment/image ID is all that's needed to retrieve its alt and title attributes.
-                                    $image_id = get_post_thumbnail_id();
-                                    $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
-                                    ?>
-                                    <img src="<?php echo the_post_thumbnail_url('full'); ?>" alt="<?php echo $image_alt ?>" srcset="" class="product-grid-image">
+                <div class="row">
+                    <?php if ($loop->have_posts()) : ?>
+                        <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+                            <div class="col-4 my-3">
+                                <div class="post-container">
+                                    <div class="image-container">
+                                        <?php
+                                        // An attachment/image ID is all that's needed to retrieve its alt and title attributes.
+                                        $image_id = get_post_thumbnail_id();
+                                        $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
+                                        ?>
+                                        <img src="<?php echo the_post_thumbnail_url('full'); ?>" alt="<?php echo $image_alt ?>" srcset="" class="product-grid-image">
 
-                                </div>
-                                <div class="post-meta text-center d-flex flex-column flex-wrap align-items-center justify-content-between">
+                                    </div>
+                                    <div class="post-meta text-center d-flex flex-column flex-wrap align-items-center justify-content-between">
 
-                                    <h2 class="product-title"> <?php the_title(); ?></h2>
+                                        <h2 class="product-title"> <?php the_title(); ?></h2>
 
-                                    <p class="post-excerpt"> <?php echo get_the_excerpt(); ?></p>
+                                        <p class="post-excerpt"> <?php echo get_the_excerpt(); ?></p>
 
-                                    <a href="<?php echo get_permalink(); ?>" target="_blank" rel="noopener noreferrer" class="know-more ">Know More</a>
+                                        <a href="<?php echo get_permalink(); ?>" target="_blank" rel="noopener noreferrer" class="know-more ">Know More</a>
+                                    </div>
                                 </div>
                             </div>
+                        <?php endwhile; ?>
+                        <div class="row my-4">
+                            <div class="col-4">
+                                <nav class="pagination">
+                                    <?php pagination_bar($loop); ?>
+                                </nav>
+                            </div>
                         </div>
-                    <?php endwhile; ?>
-                    <div class="row my-4">
-                        <div class="col-4">
-                            <nav class="pagination">
-                                <?php pagination_bar($loop); ?>
-                            </nav>
-                        </div>
-                    </div>
+                </div>
+
             </div>
 
+
+
         </div>
-
-
-
     </div>
-</div>
-<?php wp_reset_postdata(); ?>
+    <?php wp_reset_postdata(); ?>
 <?php endif; ?>
