20bc1c5a0bc199bbfac33290be5acdb5094d6cde
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-25 11:06:25 +0400

    Update product-grid.php

--- includes/product-grid.php
+++ includes/product-grid.php
@@ -1,13 +1,19 @@
-<?php
-if ($terms = get_terms(array(
-    'taxonomy' => 'product_categories',
-    'orderby' => 'name'
-))) : ?>
+<form action="<?php echo site_url() ?>/wp-admin/admin-ajax.php" method="POST" id="filter">
 
-    <ul class="category-list">
-        <?php foreach ($terms as $term) : ?>
-            <li class="prod-cats" value="<?php echo $term->term_id ?>"><?php echo $term->name ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <?php
+    if ($terms = get_terms(array(
+        'taxonomy' => 'product_categories',
+        'orderby' => 'name'
+    ))) : ?>
 
-<?php endif; ?>
+        <ul class="category-list">
+            <?php foreach ($terms as $term) : ?>
+                <li class="prod-cats" value="<?php echo $term->term_id ?>"><?php echo $term->name ?></li>
+            <?php endforeach; ?>
+        </ul>
+
+    <?php endif; ?>
+
+
+</form>
+<div id="response"></div>
