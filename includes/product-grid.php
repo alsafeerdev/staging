767c15f1a3bd31d4b1f0d061446f41f81d643efa
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-25 15:44:39 +0400

    asdad

--- includes/product-grid.php
+++ includes/product-grid.php
@@ -26,8 +26,17 @@
         </div>
         <!-- Posts Goes HERE -->
         <div class="col-md-7 posts-container">
-            
-            <div id="response"></div>
+
+            <div id="response">
+
+                <?php
+                global $wp_query; // you can remove this line if everything works for you
+
+                // don't display the button if there are not enough posts
+                if ($wp_query->max_num_pages > 1)
+                    echo '<div class="misha_loadmore">More posts</div>'; // you can use <a> as well
+                ?>
+            </div>
         </div>
     </div>
 </div>
