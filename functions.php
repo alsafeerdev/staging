2688dad5adbe33d34bcd072fe90a9f04ce29b0d3
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-25 13:03:05 +0400

    ajax in functions

--- functions.php
+++ functions.php
@@ -242,17 +242,17 @@ add_action('wp_enqueue_scripts', 'clients_carousel_css');
 function clients_carousel()
 {
     ob_start();
-    get_template_part( 'includes/clients', 'carousel' );
+    get_template_part('includes/clients', 'carousel');
     return ob_get_clean();
 }
 
-add_shortcode( 'clients-carousel', 'clients_carousel' );
+add_shortcode('clients-carousel', 'clients_carousel');
 
 // Synced Slider Gallery 
 
 function synced_slider_css()
 {
-    wp_enqueue_style('synced-slider-styles', get_stylesheet_directory_uri() . '/includes/css/min/synced-slider.min.css', true , true );
+    wp_enqueue_style('synced-slider-styles', get_stylesheet_directory_uri() . '/includes/css/min/synced-slider.min.css', true, true);
 }
 add_action('wp_enqueue_scripts', 'synced_slider_css');
 
@@ -262,7 +262,44 @@ function synced_slider_gallery()
 {
     wp_enqueue_script('synced-slider-scripts', get_stylesheet_directory_uri() . '/includes/js/min/synced-slider.min.js', array('owl-default'), true, true);
     ob_start();
-    get_template_part( 'includes/synced', 'slider' );
+    get_template_part('includes/synced', 'slider');
     return ob_get_clean();
 }
-add_shortcode( 'synced-slider', 'synced_slider_gallery' );
+add_shortcode('synced-slider', 'synced_slider_gallery');
+
+
+add_action('wp_ajax_myfilter', 'misha_filter_function'); // wp_ajax_{ACTION HERE} 
+add_action('wp_ajax_nopriv_myfilter', 'misha_filter_function');
+
+function misha_filter_function()
+{
+    $args = array(
+        'orderby' => 'date', // we will sort posts by date
+        'order'    => $_POST['date'] // ASC or DESC
+    );
+
+    // for taxonomies / categories
+    if (isset($_POST['categoryfilter']))
+        $args['tax_query'] = array(
+            array(
+                'taxonomy' => 'product_categories',
+                'key' => 'id',
+                'terms' => $_POST['categoryfilter']
+            )
+        );
+
+    // if you want to use multiple checkboxed, just duplicate the above 5 lines for each checkbox
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post();
+            echo '<h2>' . $query->post->post_title . '</h2>';
+        endwhile;
+        wp_reset_postdata();
+    else :
+        echo 'No posts found';
+    endif;
+
+    die();
+}
