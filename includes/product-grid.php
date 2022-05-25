6d21580d26b18a53970b614a2c1462f0269fcea3
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-25 14:01:49 +0400

    Update product-grid.php

--- includes/product-grid.php
+++ includes/product-grid.php
@@ -1,22 +1,32 @@
-<form action="<?php echo admin_url('admin-ajax.php') ?>" method="POST" id="filter">
-    <?php
-    if ($terms = get_terms(array(
-        'taxonomy' => 'product_categories',
-        'orderby' => 'name'
-    ))) : ?>
-        <div class="category-list">
-            <?php foreach ($terms as $term) : ?>
-                <label for="<?php echo $term->term_id ?>">
-                    <input type="radio" name="prod-category" id="<?php echo $term->term_id ?>" value="<?php echo $term->term_id ?>">
-                    <?php echo $term->name ?>
-                </label>
+<div class="container-fluid main-container">
+    <div class="row">
+        <!-- Filter Goes Here -->
+        <div class="col-md-4 filter-container">
+            <div class="filter-wrapper">
+                <form action="<?php echo admin_url('admin-ajax.php') ?>" method="POST" id="filter">
+                    <?php
+                    if ($terms = get_terms(array(
+                        'taxonomy' => 'product_categories',
+                        'orderby' => 'name'
+                    ))) : ?>
+                        <div class="category-list">
+                            <?php foreach ($terms as $term) : ?>
+                                <label for="<?php echo $term->term_id ?>">
+                                    <input type="radio" name="prod-category" id="<?php echo $term->term_id ?>" value="<?php echo $term->term_id ?>">
+                                    <?php echo $term->name ?>
+                                </label>
 
-            <?php endforeach; ?>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+                    <button class="submit-btn" type="submit">Apply Filter</button>
+                    <input type="hidden" name="action" value="myfilter">
+                </form>
+            </div>
         </div>
-    <?php endif; ?>
-    <button class="submit-btn" type="submit">Apply Filter</button>
-    <input type="hidden" name="action" value="myfilter">
-</form>
-<div id="response"></div>
-
-<!-- Query -->
+        <!-- Posts Goes HERE -->
+        <div class="col-md-08 posts-container">
+            <div id="response"></div>
+        </div>
+    </div>
+</div>
