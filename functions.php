3f8f9f5c387e4e5c5c74f8ef531d03787fdf779c
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-25 13:37:07 +0400

    awsadw

--- functions.php
+++ functions.php
@@ -268,8 +268,41 @@ function synced_slider_gallery()
 add_shortcode('synced-slider', 'synced_slider_gallery');
 
 
+// Ajax Post Filter
 
+add_action('wp_ajax_myfilter', 'ajax_filter_function'); // wp_ajax_{ACTION HERE} 
+add_action('wp_ajax_nopriv_myfilter', 'ajax_filter_function');
 
+function ajax_filter_function()
+{
+    $args = array(
+        'post_type' => 'services',
+        'orderby' => 'date', // we will sort posts by date
+        'order'    => $_POST['date'] // ASC or DESC
+    );
 
+    // for taxonomies / categories
+    if (isset($_POST['prod-category'])  )
+        $args['tax_query'] = array(
+            array(
+                'taxonomy' => 'product_categories',
+                'field' => 'id',
+                'terms' => $_POST['prod-category']
+            )
+        );
 
+    // if you want to use multiple checkboxed, just duplicate the above 5 lines for each checkbox
 
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post();
+            echo '<h2>' . $query->post->post_title . '</h2>';
+        endwhile;
+        wp_reset_postdata();
+    else :
+        echo 'No posts found';
+    endif;
+
+    die();
+}
