1af067a0047d381ba0d1907f711ca8882517784f
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-25 16:28:13 +0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -326,13 +326,13 @@ function ajax_filter_function()
 
 <?php endwhile;
         echo '</div>';
-        <?php
+      
         global $wp_query; // you can remove this line if everything works for you
 
         // don't display the button if there are not enough posts
         if ($wp_query->max_num_pages > 1)
             echo '<div class="misha_loadmore">More posts</div>'; // you can use <a> as well
-        ?>
+     
         wp_reset_postdata();
     else :
         echo 'No posts found';
