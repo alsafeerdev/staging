31907ee2a89ed083acb7bb0303ca404a60ae83cc
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-26 10:42:03 +0400

    Update cat-list.php

--- includes/cat-list.php
+++ includes/cat-list.php
@@ -6,7 +6,7 @@ $categories = get_categories( array(
 ) );
 
 foreach( $categories as $category ) {
- echo '<div class="category-list"><a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></div>';   
+ echo '<div class="category-list"><a href="' . get_category_link($category->term_id) . '">' . $category->name . ' (' . $category->count . ')</a></div>';   
 } 
 ?>
 
