a7ba3a787058ef3aa41c1cf31731a28749e4b1d9
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-05-26 09:19:53 +0400

    Update cat-list.php

--- includes/cat-list.php
+++ includes/cat-list.php
@@ -2,14 +2,15 @@
 if ($terms = get_terms(array(
     'taxonomy' => 'product_categories',
     'orderby' => 'name'
-))) : ?>
+))) :
+
+    $category_id = get_cat_ID('product_categories'); ?>
     <div class="category-list">
         <ul class="cat-list">
-            <?php $category_link = get_category_link($term->term_id); ?>
+
             <?php foreach ($terms as $term) : ?>
                 <li class="prod-cat" id="<?php echo $term->term_id ?>">
-                    <a href="<?php echo $category_link ?>" class="cat-link"></a>
-                    <?php echo $term->name ?> (<?php echo $term->count ?>)
+                    <a href="<?php get_category_link($category_id); ?> " class="cat-link"><?php echo $term->name ?> (<?php echo $term->count ?>)</a>
                 </li>
             <?php endforeach; ?>
         </ul>
