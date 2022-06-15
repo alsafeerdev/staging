45b3fe873d60e8bcd7757e548b477311a8267e61
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-06-15 12:42:54 +0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -288,6 +288,8 @@ add_action('wp_enqueue_scripts', 'image_optimize_js');
 
 function media_queries()
 {
-    wp_enqueue_style( 'media-queries', get_stylesheet_directory_uri() . '/includes/css/min/media.min.css', true );
+    wp_enqueue_style( 'media-queries', get_stylesheet_directory_uri() . '/includes/css/min/media.min.css', true , '(max-width: 1920px)' );
 
 }
+
+add_action('wp_enqueue_scripts', 'media_queries' );
