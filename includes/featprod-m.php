b8f37e9d7b47684fb9a709ab093deac32008fc54
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-06-22 12:52:42 +0400

    Update featprod-m.php

--- includes/featprod-m.php
+++ includes/featprod-m.php
@@ -1,42 +1,90 @@
-<?php
-$args = array(
-    'post_type' => 'featured_products',
-    'order' => 'ASC',
-    'orderby' => 'ID'
-);
-$loop = new WP_Query($args); ?>
-
-<div class="container">
+<div class="contaienr">
     <div class="row">
         <div class="col">
-            <div id="feat-prods-mb-carousel" class="owl-carousel owl-theme feat-prods-mb-carousel">
-                <?php if ($loop->have_posts()) : ?>
-                    <?php while ($loop->have_posts()) :
-                        $loop->the_post(); ?>
-                        <?php $feat_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>
-                        <?php
-                        $image_id = get_post_thumbnail_id();
-                        $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
-                        ?>
-                        <div class="item feat-prods-mb-container text-center">
-                            <a href="<?php echo get_permalink(); ?>" class="feat-prods-mb-wrapper-link" target="_blank">
-                                <div class="feat-prods-mb-img-container">
-                                    <img data-blink-src="<?php echo the_post_thumbnail_url('full'); ?>" alt="<?php echo $image_alt ?>" srcset="" class="feat-prods-mb-feat-image">
-                                </div>
-                                <div class="feat-prods-mb-content-container">
-                                    <h3 class="feat-prods-mb-title"> <?php the_title(); ?></h3>
-                                    <p class="feat-prods-mb-exerpt">
-                                        <?php echo get_the_excerpt(); ?>
-                                    </p>
-                                    <p class="feat-prods-mb-know-more">Learn More</p>
-                                </div>
-                            </a>
+
+            <div id="feat-prod-carousel" class="owl-carousel owl-theme feat-prod-carousel">
+                <div class="item">
+                    <a href="#" class="wrapper-link">
+                        <div class="img-container">
+                            <img src="https://ucarecdn.com/922be2d1-2307-4e9f-abcd-b406c82531dc/" alt="" srcset="">
                         </div>
-                    <?php endwhile; ?>
-                <?php endif; ?>
+                        <div class="content">
+                            <h3 class="title">
+                                KIOSK Security Portable Cabins
+                            </h3>
+                            <p class="exerpt">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                            <p class="know-more">
+                                Know More
+                            </p>
+                        </div>
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="#" class="wrapper-link">
+                        <div class="img-container">
+                            <img src="https://ucarecdn.com/680e5b15-4264-4413-839e-8ba568040dc5/" alt="" srcset="">
+                        </div>
+                        <div class="content">
+                            <h3 class="title">
+                                KIOSK Security Portable Cabins
+                            </h3>
+                            <p class="exerpt">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                            <p class="know-more">
+                                Know More
+                            </p>
+                        </div>
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="#" class="wrapper-link">
+                        <div class="img-container">
+                            <img src="https://ucarecdn.com/28ed5000-923d-4369-88b8-24ba833f2f3a/" alt="" srcset="">
+                        </div>
+                        <div class="content">
+                            <h3 class="title">
+                                KIOSK Security Portable Cabins
+                            </h3>
+                            <p class="exerpt">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                            <p class="know-more">
+                                Know More
+                            </p>
+                        </div>
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="#" class="wrapper-link">
+                        <div class="img-container">
+                            <img src="https://ucarecdn.com/c184a386-cfad-43cf-a9b5-bfb1bfb8ac38/" alt="" srcset="">
+                        </div>
+                        <div class="content">
+                            <h3 class="title">
+                                KIOSK Security Portable Cabins
+                            </h3>
+                            <p class="exerpt">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                            <p class="know-more">
+                                Know More
+                            </p>
+                        </div>
+                    </a>
+                </div>
             </div>
+
         </div>
     </div>
 </div>
-
-<?php wp_reset_postdata(); ?>
