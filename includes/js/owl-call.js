0edd63047b9d417c178862ddb721c95b0bf378b1
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-06-23 13:47:27 +0400

    asdasd

--- includes/js/owl-call.js
+++ includes/js/owl-call.js
@@ -19,6 +19,19 @@ jQuery(document).ready(function () {
     animateOut: 'fadeOut',
     animateIn: 'fadeIn',
     navText: [jQuery('.am-prev'), jQuery('.am-next')],
+    responsiveClass:true,
+    responsive: {
+      0: {
+        items: 1,
+        nav:false
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      }
+    },
   });
 });
 
