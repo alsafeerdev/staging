e4a4ac3e9af65478d5b9df2d0d8d9eb10822ce5c
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-07-03 13:20:06 +0400

    Update video-gallery.php

--- includes/video-gallery.php
+++ includes/video-gallery.php
@@ -2,4 +2,14 @@
 
 $images = get_field('video_gallery');
 
-var_dump($images);
+if ($images) :
+
+    foreach ($image as $images) : ?>
+
+<?php
+
+var_dump($image); ?>
+
+
+<?php endforeach; ?>
+<?php endif; ?>
