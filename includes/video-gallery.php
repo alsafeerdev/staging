836fb7ae1bd025a8233dcd67ed8c2c8b37dd6be5
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-07-03 13:56:19 +0400

    Update video-gallery.php

--- includes/video-gallery.php
+++ includes/video-gallery.php
@@ -10,10 +10,22 @@
         $vid_url = $image['url'];
         $vid_type = $image['mime_type'];
         $vid_width = $image['width'];
+        $vid_height = $image['height'];
         ?>
 
 
-<?php echo $vid_url . $vid_type . $vid_width ?>
+        <?php echo $vid_url . $vid_type . $vid_width ?>
+
+        <div class="vid-thumb-container">
+
+            <video width="<?php echo $vid_width ?>" height="<?php echo $vid_height ?>" controls>
+
+                <source src="<?php echo $vid_url ?>" type="<?php echo $vid_type ?>">
+                Your browser does not support the video tag.
+            </video>
+
+        </div>
+
 
     <?php endforeach; ?>
 
