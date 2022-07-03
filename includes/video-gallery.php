950cac59b696395fccaee9e72c7a38f4cabafcaf
alsafeerdev <sslmanagement.alsadv@gmail.com>
2022-07-03 14:11:20 +0400

    asdas

--- includes/video-gallery.php
+++ includes/video-gallery.php
@@ -1,33 +1,22 @@
 <?php $images = get_field('video_gallery'); ?>
 
 <?php if ($images) : ?>
-
     <div class="container">
         <div class="row">
-            <div class="vid-thumb-container">
-                <?php foreach ($images as $image) : ?>
-                    <?php
-
-                    $vid_url = $image['url'];
-                    $vid_type = $image['mime_type'];
-                    ?>
-
-                    <div class="col-md-6">
-
-                        <div class="vid-container">
-
-                            <video width="600" height="600" controls>
-
-                                <source src="<?php echo $vid_url ?>" type="<?php echo $vid_type ?>">
-                                Your browser does not support the video tag.
-                            </video>
-
-
-                        </div>
+            <?php foreach ($images as $image) : ?>
+                <?php
+                $vid_url = $image['url'];
+                $vid_type = $image['mime_type'];
+                ?>
+                <div class="col-md-6">
+                    <div class="vid-container">
+                        <video width="600" height="600" controls>
+                            <source src="<?php echo $vid_url ?>" type="<?php echo $vid_type ?>">
+                            Your browser does not support the video tag.
+                        </video>
                     </div>
-
-                <?php endforeach; ?>
-            </div>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
 <? endif; ?>
