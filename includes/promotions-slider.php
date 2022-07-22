e4c61b6f70fda41b71c2bdd97d2f205d45864afd
alsafeerdev <104498574+alsafeerdev@users.noreply.github.com>
2022-07-22 09:56:51 +0400

    cust nav

--- includes/promotions-slider.php
+++ includes/promotions-slider.php
@@ -27,13 +27,13 @@ $loop = new WP_Query($args); ?>
                         <p class="promo-exerpt letter-spacing-0-5">
                             <?php echo get_the_excerpt(); ?>
                         </p>
-                        <p class="promo-know-more letter-spacing-0-5 fw-normal color-white">Read More</p>
+                        <p class="promo-know-more letter-spacing-0-5 fw-normal color-white border-radius-30 btn-default">Read More</p>
                     </div>
                     <div class="promo-cat">
                         <?php
                         $promo_cat = get_field("promotion_category");
                         ?>
-                        <p class="promo-category-title letter-spacing-0-5 color-white"><?php echo $promo_cat; ?></p>
+                        <p class="promo-category-title letter-spacing-0-5 color-white border-radius-30"><?php echo $promo_cat; ?></p>
                     </div>
                 </a>
             </div>
@@ -41,7 +41,7 @@ $loop = new WP_Query($args); ?>
     <?php endif; ?>
 </div>
 <!-- Custom Navigation -->
-<div class="next-btn promo-cust-nav">
+<div class="next-btn cust-nav">
     <svg version="1.1" id="nav-next" class="promo-nav-main svg-white-nav" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 592 1000" style="enable-background:new 0 0 592 1000;" xml:space="preserve">
         <path d="M585.8,463.7c-4.8-12.2-11.9-23.1-21.2-32.4L161,27.7C143.1,9.8,119.4,0,94.2,0C69,0,45.3,9.8,27.4,27.7
 	C9.6,45.5-0.2,69.2-0.2,94.4c0,25.2,9.8,48.9,27.7,66.8L366,499.8L27.4,838.3C9.6,856.2-0.2,879.9-0.2,905.1
@@ -50,7 +50,7 @@ $loop = new WP_Query($args); ?>
     </svg>
 
 </div>
-<div class="prev-btn rotate-180 promo-cust-nav">
+<div class="prev-btn rotate-180 cust-nav">
     <svg version="1.1" id="nav-prev" class="promo-nav-main svg-white-nav" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 592 1000" style="enable-background:new 0 0 592 1000;" xml:space="preserve">
         <path d="M585.8,463.7c-4.8-12.2-11.9-23.1-21.2-32.4L161,27.7C143.1,9.8,119.4,0,94.2,0C69,0,45.3,9.8,27.4,27.7
 	C9.6,45.5-0.2,69.2-0.2,94.4c0,25.2,9.8,48.9,27.7,66.8L366,499.8L27.4,838.3C9.6,856.2-0.2,879.9-0.2,905.1
