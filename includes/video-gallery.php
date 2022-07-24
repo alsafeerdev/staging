e755d872ccce4c7e1275b4e0c1cfa15e82d1ba9e
alsafeerdev <104498574+alsafeerdev@users.noreply.github.com>
2022-07-24 14:23:45 +0400

    Update video-gallery.php

--- includes/video-gallery.php
+++ includes/video-gallery.php
@@ -1,28 +1,22 @@
-<!-- NEW -->
-<?php $rows = get_field('video_gallery'); ?>
-
-<?php if ($rows) : ?>
-    <div class="container mb-5">
-        <div class="row">
-            <?php foreach ($rows as $row) : ?>
-
-                <?php
-
-                $vid_webm = $rows[0];
-                $vid_mp4 = $rows[1];
-                ?>
-                <div class="col-md-6 mb-3">
-                    <div class="vid-container" style="width:fit-content">
-                        <video width="600" controls controlsList="nodownload">
-                            <source src="<?php echo $vid_webm; ?>" type="video/webm">
-                            <source src="<?php echo $vid_mp4; ?>" type="video/mp4">
-                            Your browser does not support the video tag.
-                        </video>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </div>
-<? endif; ?>
-
 <!-- NEW END -->
+<?php
+
+if (have_rows('video_gallery')) :
+    while (have_rows('video_gallery')) : the_row();
+
+        // Layout 1.
+        $webm = get_sub_field('webm');
+        $mp4 = get_sub_field('mp4'); ?>
+
+        <div class="col-md-4 mb-3">
+            <div class="vid-container" style="width:fit-content">
+                <video width="600" controls controlsList="nodownload">
+                    <source src="<?php echo $webm; ?>" type="video/webm">
+                    <source src="<?php echo $mp4; ?>" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div>
+
+<?php endwhile;
+endif; ?>
