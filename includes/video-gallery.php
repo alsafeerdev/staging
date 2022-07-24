06fcf36e801e4a819cd8b04b48d0bb73fca129e8
alsafeerdev <104498574+alsafeerdev@users.noreply.github.com>
2022-07-24 15:50:30 +0400

    Update video-gallery.php

--- includes/video-gallery.php
+++ includes/video-gallery.php
@@ -6,7 +6,6 @@
                 $webm = get_sub_field('webm');
                 $mp4 = get_sub_field('mp4');
                 $fallback = get_sub_field('fallback_image') ?>
-
                 <div class="col-md-4 mb-3">
                     <div class="vid-container" style="width:fit-content">
                         <video width="600" style="border-radius: 10px;" controls controlsList="nodownload" poster="<?php echo $fallback; ?>">
