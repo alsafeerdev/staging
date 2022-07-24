f94f4ca0cd34ca512726adbd20551ac73b64bbd6
alsafeerdev <104498574+alsafeerdev@users.noreply.github.com>
2022-07-24 14:26:36 +0400

    Update video-gallery.php

--- includes/video-gallery.php
+++ includes/video-gallery.php
@@ -1,22 +1,25 @@
-<!-- NEW END -->
-<?php
+<div class="container mb-5">
+    <div class="row">
+        <?php
 
-if (have_rows('video_gallery')) :
-    while (have_rows('video_gallery')) : the_row();
+        if (have_rows('video_gallery')) :
+            while (have_rows('video_gallery')) : the_row();
 
-        // Layout 1.
-        $webm = get_sub_field('webm');
-        $mp4 = get_sub_field('mp4'); ?>
+                // Layout 1.
+                $webm = get_sub_field('webm');
+                $mp4 = get_sub_field('mp4'); ?>
 
-        <div class="col-md-4 mb-3">
-            <div class="vid-container" style="width:fit-content">
-                <video width="600" controls controlsList="nodownload">
-                    <source src="<?php echo $webm; ?>" type="video/webm">
-                    <source src="<?php echo $mp4; ?>" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video>
-            </div>
-        </div>
+                <div class="col-md-4 mb-3">
+                    <div class="vid-container" style="width:fit-content">
+                        <video width="600" controls controlsList="nodownload">
+                            <source src="<?php echo $webm; ?>" type="video/webm">
+                            <source src="<?php echo $mp4; ?>" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </div>
 
-<?php endwhile;
-endif; ?>
+        <?php endwhile;
+        endif; ?>
+    </div>
+</div>
