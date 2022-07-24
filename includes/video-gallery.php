be291cd026d911a8d92d2d7b0bfbfacaa23b2ad5
alsafeerdev <104498574+alsafeerdev@users.noreply.github.com>
2022-07-24 13:39:51 +0400

    Update video-gallery.php

--- includes/video-gallery.php
+++ includes/video-gallery.php
@@ -1,17 +1,21 @@
-<?php $images = get_field('video_gallery'); ?>
+<!-- NEW -->
+<?php $rows = get_field('video_gallery'); ?>
 
-<?php if ($images) : ?>
+<?php if ($rows) : ?>
     <div class="container mb-5">
         <div class="row">
-            <?php foreach ($images as $image) : ?>
+            <?php foreach ($rows as $row) : ?>
+
                 <?php
-                $vid_url = $image['url'];
-                $vid_type = $image['mime_type'];
+
+                $vid_webm = get_sub_field('webm');
+                $vid_mp4 = get_sub_field('mp4');
                 ?>
                 <div class="col-md-6 mb-3">
                     <div class="vid-container" style="width:fit-content">
                         <video width="600" controls controlsList="nodownload">
-                            <source src="<?php echo $vid_url ?>" type="<?php echo $vid_type ?>">
+                            <source src="<?php echo $vid_webm ?>" type="video/webm">
+                            <source src="<?php echo $vid_mp4 ?>" type="video/mp4">
                             Your browser does not support the video tag.
                         </video>
                     </div>
@@ -20,3 +24,6 @@
         </div>
     </div>
 <? endif; ?>
+
+
+<!-- NEW END -->
