84913f973e1190c614f03b3872109c3f3f91ef5a
alsafeerdev <104498574+alsafeerdev@users.noreply.github.com>
2022-07-24 14:18:49 +0400

    Update video-gallery.php

--- includes/video-gallery.php
+++ includes/video-gallery.php
@@ -9,13 +9,13 @@
                 <?php
 
                 $vid_webm = $rows[0];
-                $vid_mp4 = $rows[1];
+                $vid_webm = $rows[1];
                 ?>
                 <div class="col-md-6 mb-3">
                     <div class="vid-container" style="width:fit-content">
                         <video width="600" controls controlsList="nodownload">
-                            <source src="<?php echo esc_url($vid_webm['url']); ?>" type="video/webm">
-                            <source src="<?php echo esc_url($vid_mp4['url']); ?>" type="video/mp4">
+                            <source src="<?php echo $vid_webm; ?>" type="video/webm">
+                            <source src="<?php echo $vid_webm; ?>" type="video/mp4">
                             Your browser does not support the video tag.
                         </video>
                     </div>
