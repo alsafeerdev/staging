76512d847e09523799fe86c52c2104ae877e0d8d
alsafeerdev <104498574+alsafeerdev@users.noreply.github.com>
2022-07-25 09:21:22 +0400

    Update promotions-sliderLightBG.php

--- includes/promotions-sliderLightBG.php
+++ includes/promotions-sliderLightBG.php
@@ -41,7 +41,7 @@ $loop = new WP_Query($args); ?>
     <?php endif; ?>
 </div>
 <!-- Custom Navigation -->
-<div class="next-btn-light promo-cust-nav">
+<div class="next-btn-light cust-nav">
     <svg version="1.1" id="nav-next" class="svg-dark-nav" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 592 1000" style="enable-background:new 0 0 592 1000;" xml:space="preserve">
         <path d="M585.8,463.7c-4.8-12.2-11.9-23.1-21.2-32.4L161,27.7C143.1,9.8,119.4,0,94.2,0C69,0,45.3,9.8,27.4,27.7
 	C9.6,45.5-0.2,69.2-0.2,94.4c0,25.2,9.8,48.9,27.7,66.8L366,499.8L27.4,838.3C9.6,856.2-0.2,879.9-0.2,905.1
@@ -50,7 +50,7 @@ $loop = new WP_Query($args); ?>
     </svg>
 
 </div>
-<div class="prev-btn-light rotate-180 promo-cust-nav">
+<div class="prev-btn-light rotate-180 cust-nav">
     <svg version="1.1" id="nav-prev" class="svg-dark-nav" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 592 1000" style="enable-background:new 0 0 592 1000;" xml:space="preserve">
         <path d="M585.8,463.7c-4.8-12.2-11.9-23.1-21.2-32.4L161,27.7C143.1,9.8,119.4,0,94.2,0C69,0,45.3,9.8,27.4,27.7
 	C9.6,45.5-0.2,69.2-0.2,94.4c0,25.2,9.8,48.9,27.7,66.8L366,499.8L27.4,838.3C9.6,856.2-0.2,879.9-0.2,905.1
